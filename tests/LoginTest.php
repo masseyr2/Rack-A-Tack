7cd902e85533fa90b13d0c7a97c08eda568c670d
brownl13 <brownl13@nku.edu>
2018-03-27 16:44:56 -0400

    tests for staff login

--- tests/LoginTest.php
+++ tests/LoginTest.php
@@ -1,52 +1,71 @@
-<?PHP
-use PHPUnit\Framework\TestCase;
-include("db_connect.php");
-
-Class LoginTest extends TestCase
-{
-	/**
-     * @covers Login::IsInvalidUsername
-     */
-	public function testGivenInvalid_WhenInvalidUsername_Then()
-	{
-		
-		$SUT = 'Invalid';
-		$login = new Login();
-		$this->assertEquals(True, $login->isInvalidUsername('Invalid'));
-		
-	}	
-	/**
-     * @covers Login::IsInvalidUsername
-     */
-	public function testGivenValid_WhenInvalidUsername_ThenFalse()
-	{
-		
-		$SUT = "Valid";
-		$login = new Login();
-		$this->assertEquals(False, $login->isInvalidUsername($SUT));
-	}
-
-	/**
-     * @covers Login::IsInvalidPassword
-     */
-	public function testGivenInvalid_WhenInvalidPassword_ThenTrue()
-	{
-		$username = "Logan";
-		$SUT = "invalid";
-		$login = new Login();
-	    $this->assertEquals(True, $login->isInvalidPassword($SUT, $username));
-	}
-	
-	/**
-     * @covers Login::IsInvalidPassword
-     */
-	public function testGivenValid_WheninvalidPassword_ThenFalse()
-	{
-		$username = "Logan";
-		$SUT = "Valid";
-		$login = new Login();
-		$this->assertEquals(False, $login->isInvalidPassword($SUT, $username));
-	}	
-}
-
-?>
+<?PHP
+use PHPUnit\Framework\TestCase;
+include("db_connect.php");
+
+Class LoginTest extends TestCase
+{
+	/**
+     * @covers Login::IsInvalidUsername
+     */
+	public function testGivenInvalid_WhenInvalidUsername_ThenTrue()
+	{
+		
+		$username = 'Invalid';
+		$SUT = new Login();
+		$this->assertEquals(True, $SUT->isInvalidUsername($username));
+		
+	}	
+	/**
+     * @covers Login::IsInvalidUsername
+     */
+	public function testGivenValid_WhenInvalidUsername_ThenFalse()
+	{
+		
+		$username = "Valid";
+		$SUT = new Login();
+		$this->assertEquals(False, $SUT->isInvalidUsername($username));
+	}
+
+	/**
+     * @covers Login::IsInvalidPassword
+     */
+	public function testGivenInvalid_WhenInvalidPassword_ThenTrue()
+	{
+		$username = "Logan";
+		$password = "invalid";
+		$SUT = new Login();
+	    $this->assertEquals(True, $SUT->isInvalidPassword($password, $username));
+	}
+	
+	/**
+     * @covers Login::IsInvalidPassword
+     */
+	public function testGivenValid_WheninvalidPassword_ThenFalse()
+	{
+		$username = "Logan";
+		$password = "Valid";
+		$SUT = new Login();
+		$this->assertEquals(False, $SUT->isInvalidPassword($password, $username));
+	}
+	/**
+	 * @covers Login::IsInvalidStaffUsername
+	 */
+	public function testGivenLogan_WhenInvalidStaffUsername_ThenTrue()
+	{
+		$username = "Logan";
+		$SUT = new Login();
+	    $this->assertEquals(True, $SUT->isInvalidStaffUsername($username));
+	}
+	
+	/**
+	 * @covers Login::IsInvalidStaffUsername
+	 */
+	public function testGivenMassey_WhenInvalidStaffUsername_ThenFalse()
+	{
+		$username = "Massey";
+		$SUT = new Login();
+		$this->assertEquals(False, $SUT->isInvalidStaffUsername($username));
+	}	
+}
+
+?>
